673c85650e28901db1739d35a20c7e15a6cef2dc
Felipe Costa <speratifelipe1@gmail.com>
2024-10-08 01:02:21 -0300

    Versão Final

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
+
 let arrayAmigos = [];
-let numeroDeAmigos = 0
+let numeroDeAmigos = 0;
+let listaDeAmigos = document.getElementById('lista-amigos');
+let listaSorteio = document.getElementById('lista-sorteio');
 
 function adicionar () {
 
     let nomeDoAmigo = document.getElementById('nome-amigo').value;
-    let listaDeAmigos = document.getElementById('lista-amigos');
+
     if (listaDeAmigos.textContent == '') {
         listaDeAmigos.textContent = nomeDoAmigo;
     } else {
@@ -19,18 +22,29 @@ function adicionar () {
 function sortear () {
     let i = 0;
     let numerosSorteados = []
+    listaSorteio.textContent = '';
     while (i < numeroDeAmigos) {
         let indiceSorteio = Math.floor(Math.random() * numeroDeAmigos);
-        if (!numerosSorteados.includes(indiceSorteio)) {
+        if (!numerosSorteados.includes(indiceSorteio) && arrayAmigos[i] != arrayAmigos[indiceSorteio]) {
             numerosSorteados.push(indiceSorteio);
             i++;
+        } else if (i == arrayAmigos.length - 1 && arrayAmigos[i] == arrayAmigos[indiceSorteio]) {
+            sortear ();
+            return;
         }
     }
     console.log(numerosSorteados);
+
+    for (i = 0; i < numerosSorteados.length; i++) {
+        let nomesListaSorteio = `${arrayAmigos[i]} --> ${arrayAmigos[numerosSorteados[i]]}`
+        listaSorteio.innerHTML += `<p>${nomesListaSorteio}</p>`;
+    }
+
 }
 
 function reiniciar () {
     arrayAmigos = [];
     document.getElementById('lista-amigos').textContent = '';
-    numeroDeAmigos = 0
+    numeroDeAmigos = 0;
+    listaSorteio.textContent = '';
 }
