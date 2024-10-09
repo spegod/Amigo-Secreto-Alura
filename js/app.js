2035bcd815fb09d3a1ccdf869578e75b17f6ce9f
Felipe Costa <speratifelipe1@gmail.com>
2024-10-09 18:03:31 -0300

    Remover Nomes da Lista

--- js/app.js
+++ js/app.js
@@ -3,25 +3,28 @@ let arrayAmigos = [];
 let numeroDeAmigos = 0;
 let listaDeAmigos = document.getElementById('lista-amigos');
 let listaSorteio = document.getElementById('lista-sorteio');
+let childrenAmigos = listaDeAmigos.children;
 
 function adicionar () {
 
     let nomeDoAmigo = document.getElementById('nome-amigo').value;
 
     if (listaDeAmigos.textContent == '') {
-        listaDeAmigos.textContent = nomeDoAmigo;
+        //listaDeAmigos.textContent = nomeDoAmigo;
+        listaDeAmigos.innerHTML = `<a onclick="removerAmigo('${nomeDoAmigo}')" href="#" id="${nomeDoAmigo}" class="form__link">${nomeDoAmigo}</a>`;
     } else {
-        listaDeAmigos.textContent += `, ${nomeDoAmigo}`;
+        //listaDeAmigos.textContent += `, ${nomeDoAmigo}`;
+        listaDeAmigos.innerHTML += `<a onclick="removerAmigo('${nomeDoAmigo}')" href="#" id="${nomeDoAmigo}" class="form__link">, ${nomeDoAmigo}</a>`;
     }
     document.getElementById('nome-amigo').value = '';
     arrayAmigos.push(nomeDoAmigo);
     console.log(arrayAmigos);
-    numeroDeAmigos++
+    numeroDeAmigos++;
 }
 
 function sortear () {
     let i = 0;
-    let numerosSorteados = []
+    let numerosSorteados = [];
     listaSorteio.textContent = '';
     while (i < numeroDeAmigos) {
         let indiceSorteio = Math.floor(Math.random() * numeroDeAmigos);
@@ -36,7 +39,7 @@ function sortear () {
     console.log(numerosSorteados);
 
     for (i = 0; i < numerosSorteados.length; i++) {
-        let nomesListaSorteio = `${arrayAmigos[i]} --> ${arrayAmigos[numerosSorteados[i]]}`
+        let nomesListaSorteio = `${arrayAmigos[i]} --> ${arrayAmigos[numerosSorteados[i]]}`;
         listaSorteio.innerHTML += `<p>${nomesListaSorteio}</p>`;
     }
 
@@ -47,4 +50,14 @@ function reiniciar () {
     document.getElementById('lista-amigos').textContent = '';
     numeroDeAmigos = 0;
     listaSorteio.textContent = '';
+}
+
+function removerAmigo (nome) {
+    let amigoRemovido = document.getElementById(`${nome}`);
+    amigoRemovido.remove();
+    arrayAmigos = arrayAmigos.filter(function(amigo) {
+        return amigo !== nome;
+    })
+    numeroDeAmigos--
+    console.log(arrayAmigos);
 }
